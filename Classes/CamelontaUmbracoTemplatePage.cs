548602d2e9669492ffb915ea5c3813c08a41a624
tobias <tobias@d252d4b6-3862-8246-84e1-cf8c128d3774>
2015-03-16 12:15:22 +0000

    Boilerplate: * Använder ID:t för top-nav * Hjälpklass att ärva ifrån för att återanvända egenskaper och metoder

---
+++ Classes/CamelontaUmbracoTemplatePage.cs
@@ -0,0 +1,20 @@
+﻿using Umbraco.Web.Mvc;
+
+namespace Camelonta.Boilerplate.Classes
+{
+    public abstract class CamelontaUmbracoTemplatePage : UmbracoTemplatePage
+    {
+        public dynamic CurrentSite
+        {
+            get
+            {
+                if (_currentSite == null)
+                {
+                    _currentSite = CurrentPage.AncestorOrSelf(1);
+                }
+                return _currentSite;
+            }
+        }
+        private dynamic _currentSite;
+    }
+}
