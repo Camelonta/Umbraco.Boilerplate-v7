87e6c087037c001706ca96aba924425ddd2cdff5
tobias <tobias@d252d4b6-3862-8246-84e1-cf8c128d3774>
2015-03-20 07:57:41 +0000

    Boilerplate: ExtensionMethod för ShowOnlyVisible

---
+++ Classes/ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Web.Models;
+
+namespace Camelonta.Boilerplate.Classes
+{
+    public static class ExtensionMethods
+    {
+        public static IEnumerable<DynamicPublishedContent> ShowOnlyVisible(this DynamicPublishedContentList pages)
+        {
+            return pages.Where(p => p.Visible);
+        }
+    }
+}
