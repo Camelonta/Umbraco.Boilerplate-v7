fe66a377dde8cd34ea6195edecce229665c0c73c
sebastian <sebastian@d252d4b6-3862-8246-84e1-cf8c128d3774>
2015-03-25 12:23:46 +0000

    Boilerplate - Uppgraderade till Umbraco 7.2.4

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,6 +1,6 @@
 camelonta.swiper =  new Swiper ('.swiper-container', {
     loop: true,
-    autoplay: 5000,
+    autoplay: 10000,
     centeredSlides: true,
     pagination: '.swiper-pagination',
     paginationClickable: '.swiper-pagination',
