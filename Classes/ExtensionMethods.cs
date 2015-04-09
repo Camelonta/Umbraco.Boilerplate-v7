b8cf1d2e11d412a316b688d5309eed7ab1fae7ac
sebastian <sebastian@d252d4b6-3862-8246-84e1-cf8c128d3774>
2015-04-09 06:13:11 +0000

    Boilerplate - Använder nu NavName i menyer

--- Classes/ExtensionMethods.cs
+++ Classes/ExtensionMethods.cs
@@ -1,7 +1,13 @@
-﻿namespace Camelonta.Boilerplate.Classes
+﻿using Umbraco.Core.Models;
+namespace Camelonta.Boilerplate.Classes
 {
     public static class ExtensionMethods
     {
-
+        public static string NavName(this IPublishedContent page)
+        {
+            if (page.GetProperty("navName").HasValue)
+                return page.GetProperty("navName").Value.ToString();
+            return page.Name;
+        }
     }
 }
