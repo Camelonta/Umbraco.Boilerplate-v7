f9f529d8566b1d4e0f2ddd2b35139a6965753e1a
jesse <jesse@d252d4b6-3862-8246-84e1-cf8c128d3774>
2015-04-10 13:53:42 +0000

    Boilerplate: - fixat till youtubevideo lite. Man måste inte ange höjd. Den behåller proportionerna och är responsiv

--- scripts/youtube.js
+++ scripts/youtube.js
@@ -2,13 +2,10 @@ camelonta.youtube = (function () {
 
     // Init scripts
     var init = function () {
-        $(".youtube-preview").click(function () {
-            var videoId = $(this).data("video");
-            if (videoId) {
-                var url = "http://www.youtube.com/embed/" + videoId + "?modestbranding=1&autohide=1&showinfo=0&autoplay=1&rel=0";
-                var iframe = '<iframe src="' + url + '" allowfullscreen style="width:100%; height:100%; border:0"></iframe>';
-                $(this).html(iframe);
-            }
+        $(".video").click(function () {
+            var url = "http://www.youtube.com/embed/" + $(this).data("video-id") + "?modestbranding=1&autohide=1&showinfo=0&autoplay=1&rel=0";
+            var iframe = '<iframe src="' + url + '" allowfullscreen style="width:100%; height:100%; border:0"></iframe>';
+            $(this).html(iframe);
         });
     }
 
