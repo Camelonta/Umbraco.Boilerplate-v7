1a581b2a4e5dd45f28e6080c2575be3c03737f7d
sebastian <sebastian@d252d4b6-3862-8246-84e1-cf8c128d3774>
2015-04-14 13:53:57 +0000

    Boilerplate * Använder ExtensionMethods från Camelonta.Utilities för menyerna * Kortare README.txt * Använder pageOrganizer från Camelonta.Utilities
    Camelonta.Utilities
    * ExtensionMethods för IfPageIsActive/IfPageIsCurrent

--- App_Start/DateFolderEventHandler.cs
+++ App_Start/DateFolderEventHandler.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Umbraco.Core;
+﻿using Umbraco.Core;
 using Umbraco.Core.Events;
 using Umbraco.Core.Models;
 using Umbraco.Core.Services;
@@ -19,48 +18,8 @@ namespace Camelonta.Boilerplate.App_Start
         private void ContentService_Saved(IContentService sender, SaveEventArgs<IContent> e)
         {
             var contentService = ApplicationContext.Current.Services.ContentService;
-            foreach (var page in e.SavedEntities)
-            {
-                // Not interested in anything but "create" events.
-                if (!page.IsNewEntity()) return;
-
-                // Not interested if the item being added is not a news-page.
-                if (page.ContentType.Alias != "News") return;
-
-                var now = page.ReleaseDate.HasValue ? page.ReleaseDate.Value : DateTime.Now;
-                var year = now.ToString("yyyy");
-                var month = now.ToString("MM");
-
-                IContent yearDocument = null;
-
-                // Get year-document by container (if it is a 4 digit number)
-                int n;
-                if (int.TryParse(page.Parent().Name, out n))
-                {
-                    if (n.ToString().Length == 4)
-                        yearDocument = page.Parent();
-                }
-                // Get year-document by parent-siblings
-                if (yearDocument != null)
-                    foreach (var child in page.Parent().Children())
-                    {
-                        if (child.Name == year)
-                        {
-                            yearDocument = child;
-                            break;
-                        }
-                    }
-
-                // If the year folder doesn't exist, create it.
-                if (yearDocument == null)
-                {
-                    yearDocument = contentService.CreateContentWithIdentity(year, page.ParentId, "NewsList");
-                    contentService.Publish(yearDocument);
-                }
-
-                // Move the document into the year folder
-                contentService.Move(page, yearDocument.Id);
-            }
+            var pageOrganizer = new Camelonta.Utilities.PageOrganizer();
+            pageOrganizer.MoveToDatefolder(e, contentService, "News", "NewsList");
         }
     }
 }
--- README.txt
+++ README.txt
@@ -1,28 +1,19 @@
 TIPS: Checka in ofta så man kan gå tillbaks ifall något går fel
 
-1. Skapa ett nytt projekt (ASP.NET Web Application). Välj empty, kryssa inte i några checkboxar (blir konflikt vid installation av Umbraco ifall man tar MVC)
-
-2. Om du lägger till test-projekt och kör Visual Studio 2013 så kan det hamna på fel ställe. 
+OBS! Om du lägger till test-projekt och kör Visual Studio 2013 så kan det hamna på fel ställe. 
    I så fall: Kopiera hela mappen och ändra till rätt sökväg i .sln-filen. Stäng projektet och öppna det på nytt.
 
-3. Checka in alla filer som ska checkas in. Om något går fel slipper vi göra om dessa steg.
 
-4. Installera Umbraco CMS via NuGet
+1. Skapa ett nytt projekt (ASP.NET Web Application). Välj empty, kryssa inte i några checkboxar (blir konflikt vid installation av Umbraco ifall man tar MVC)
 
-5. Checka in alla filer som ska checkas in. Om något går fel kan vi bara rensa och börja om här.
+2. Installera "Camelonta Umbraco Metapaket"  via NuGet
 
-6. Starta projektet och kör Umbraco installationen OBS: Välj Customize för att slippa deras standard-mall
+3. Starta projektet och kör Umbraco installationen OBS: Välj Customize för att slippa deras standard-mall
 
-7. Installera Boilerplate-paketet
+4. Installera Boilerplate-paketet (exporterat ifrån "Umbraco admin -> Utvecklare -> Paket" i Boilerplate-projektet)
 
-8. För att bundling av css/js filer ska fungera så lägg till ~/bundles på umbracoReservedPaths i web.config och installera Microsoft.AspNet.Web.Optimization från nuget
+5. Byt namn på ditt namespace
 
-9. Byt namn på ditt namespace
+6. Inkludera allt i App_Start, Views, Classes, css, img och scripts i ditt projekt
 
-10. Inkludera allt i App_Start, Views, Classes, css, img och scripts i ditt projekt
-
-11. Sätt umbracoTimeOutInMinutes i web.config till tex 1440 (24 timmar) för att slippa bli utloggad
-
-12. I Umbracos UI: Peka ut "Home" (eller egen förstasida) i rotnoden (sidträdet) -> fliken Properties -> umbracoInternalRedirectId på Site-noden
-
-13. Installera önskade Camelonta-paket + Url Tracker
+7. I Umbracos UI: Peka ut "Home" (eller egen förstasida) i rotnoden (sidträdet) -> fliken Properties -> umbracoInternalRedirectId på Site-noden
