4b4c660c538dd71631c75185345e7ea92ae4ab60
jesse <jesse@d252d4b6-3862-8246-84e1-cf8c128d3774>
2015-12-30 08:44:16 +0000

    boierplate: - cookievarning gör inga requests mer + visas inte om den är tom - från px till em på alla fonter

--- scripts/cookie-warning.js
+++ scripts/cookie-warning.js
@@ -3,9 +3,15 @@
 
     if (!Cookies.get(cookieName)) {
         $.post('/umbraco/surface/partialsurface/cookiewarning', { nodeId: window.currentNode }, function (data) {
-            var html = $(data);
-            $('body').prepend(html);
-            html.slideDown();
+
+            if (data.length > 10) {
+                var html = $(data);
+                $('body').prepend(html);
+                html.slideDown();
+            } else {
+                // Disclaimer does not contain any text. Disable it so it doesn't make any more requests
+                Cookies.set(cookieName, true, { expires: 365 });
+            }
 
             // Click-event on Accept-button
             $('#cookie-warning .button').on('click', function () {
