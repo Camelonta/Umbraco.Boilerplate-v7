94df807e520a9dfe8da8bc319d4b9dda0b5266a2
tobias <tobias@d252d4b6-3862-8246-84e1-cf8c128d3774>
2016-02-15 09:55:54 +0000

    Boilerplate: * Uppdatering av Camelonta UI (filer jag måste missat att checka in)

--- App_Plugins/Camelonta.UI/camelontacms.js
+++ App_Plugins/Camelonta.UI/camelontacms.js
@@ -1,119 +1,52 @@
-﻿(function () {
+﻿angular.module('umbraco.services').config(['$httpProvider', function ($httpProvider) {
 
-    // Globals
-    var loaded;
+    $httpProvider.interceptors.push(function ($q) {
+        return {
+            'response': function (response) {
 
-    // Init
-    function init() {
-        loaded = {}
-        setTimeout(loadLoginPage, 300);
-        setTimeout(loadCamelontaLogo, 300);
-        setTimeout(loadHelpLink, 400);
-        setTimeout(loadUserLink, 300);
-    }
-    $(function () {
-        init();
+                // Load our welcome-text on the login-page
+                if (response.config.url === 'views/common/dialogs/login.html') {
+                    loadLoginPage(response);
+                }
+                // Load camelonta-logo
+                if (response.config.url === 'views/components/application/umb-sections.html') {
+                    loadCamelontaLogo(response);
+                }
+                // Load our content in help-section
+                if (response.config.url === 'views/common/overlays/help/help.html') {
+                    loadHelpSection(response);
+                }
+
+                return response;
+            }
+        };
     });
 
+    // Load our welcome-text on the login-page
+    function loadLoginPage(response) {
+        var html = $('<div />', { html: response.data });
+        html.find("#login h1")
+            .html("Välkommen till <span class='cam-green'>Camelonta Web Pro</span> <span class='cam-basedon'>- en tjänst baserad på <span class='cam-orange'>Umbraco</span> CMS</span>")
+            .css('opacity', 1);
 
-    // Load camelonta-logo
-    function loadCamelontaLogo() {
+        html.find("#login").prepend("<div class='camelonta-logo'><a href='/umbraco'><img src='../App_Plugins/Camelonta.UI/camelontacms.png'/></a></div>");
 
-        if ($("#applications .sections").length) {
-
-            $("#applications .sections").prepend("<li class='camelonta-logo'><a href='/umbraco'><img src='../App_Plugins/Camelonta.UI/camelontacms.png'/></a></li>");
-
-            $("li.avatar").detach().insertBefore('li.help');
-
-            loaded.camelontaLogoLoaded = true;
-        }
-
-        if (!loaded.camelontaLogoLoaded)
-            setTimeout(loadCamelontaLogo, 300);
+        response.data = html.html();
     }
 
-    // Load click-event to help-section
-    function loadHelpLink() {
-
-        if ($("a.help").length) {
-
-            $("a.help").click(function () {
-                setTimeout(loadHelpSection, 300);
-            });
-
-            loaded.helpLinkLoaded = true;
-        }
-
-        if (!loaded.helpLinkLoaded)
-            setTimeout(loadHelpLink, 300);
+    // Load camelonta-logo
+    function loadCamelontaLogo(response) {
+        var html = $('<div />', { html: response.data });
+        html.find("#applications .sections").prepend("<li class='camelonta-logo'><a href='/umbraco'><img src='../App_Plugins/Camelonta.UI/camelontacms.png'/></a></li>")
+        html.find("li.avatar").insertBefore(html.find("li.help"));
+        response.data = html.html();
     }
 
     // Load our content in help-section
-    function loadHelpSection() {
-
-        if ($("div[ng-controller='Umbraco.Dialogs.HelpController']").length) {
-
-            $("div[ng-controller='Umbraco.Dialogs.HelpController'] .tab-content > .umb-pane:first-child").prepend("<iframe height='240' src='../App_Plugins/Camelonta.UI/help.html'/>");
-
-            loaded.helpSectionLoaded = true;
-        }
-
-        if (!loaded.helpSectionLoaded)
-            setTimeout(loadHelpSection, 300);
+    function loadHelpSection(response) {
+        var html = $('<div />', { html: response.data });
+        html.find(".umb-control-group:first").prepend("<iframe height='240' src='../App_Plugins/Camelonta.UI/help.html'/>");
+        response.data = html.html();
     }
 
-    // Load our welcome-text on the login-page
-    function loadLoginPage() {
-
-        if ($("#login h1").length) {
-
-            $("#login h1")
-                .html("Välkommen till <span class='cam-green'>Camelonta Web Pro</span> <span class='cam-basedon'>- en tjänst baserad på <span class='cam-orange'>Umbraco</span> CMS</span>")
-                .css('opacity', 1);
-
-            $("#login").prepend("<div class='camelonta-logo'><a href='/umbraco'><img src='../App_Plugins/Camelonta.UI/camelontacms.png'/></a></div>");
-
-            loaded.loginPageLoaded = true;
-        }
-
-        // Check if we are not on loginpage and abort
-        if ($("#leftcolumn #applications").length) {
-            loaded.loginPageLoaded = true;
-        }
-
-        if (!loaded.loginPageLoaded)
-            setTimeout(loadLoginPage, 300);
-    }
-
-    // Load click-event to user-profile-section
-    function loadUserLink() {
-
-        if ($("#avatar-img").length) {
-
-            $("#avatar-img").click(function () {
-                setTimeout(loadUserSection, 300);
-            });
-
-            loaded.userLinkLoaded = true;
-        }
-
-        if (!loaded.userLinkLoaded)
-            setTimeout(loadUserLink, 300);
-    }
-
-    // Load click-event on logout-button in user-section
-    function loadUserSection() {
-        if ($("div[ng-controller='Umbraco.Dialogs.UserController']").length) {
-
-            $("div[ng-controller='Umbraco.Dialogs.UserController'] .btn-toolbar button").click(function () {
-                init();
-            });
-
-            loaded.userSectionLoaded = true;
-        }
-
-        if (!loaded.userSectionLoaded)
-            setTimeout(loadUserSection, 300);
-    }
-
-})();
+}]);
