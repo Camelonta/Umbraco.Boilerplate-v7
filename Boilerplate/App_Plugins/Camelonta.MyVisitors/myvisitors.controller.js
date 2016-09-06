8c1cb9e00f7917428f3ab439f2d6eb49eb5c0fcf
jesse <jesse@d252d4b6-3862-8246-84e1-cf8c128d3774>
2016-09-06 08:25:39 +0000

    boilerplate: - all navigering kör BEM-struktur. Lättare att styla specifika situationer & navigeringar m.m. (mer kod dock. Men ändå win) - aktuellt-sidan kör BEM + lite snyggare lättläst kod

--- Boilerplate/App_Plugins/Camelonta.MyVisitors/myvisitors.controller.js
+++ Boilerplate/App_Plugins/Camelonta.MyVisitors/myvisitors.controller.js
@@ -1,4 +1,8 @@
-angular.module("umbraco").controller("MyVisitors.DashController", function ($scope, $http, $window, $element) {
+console.log(app.requires)
+//This is my Angular module that I want to inject/require
+app.requires.push('chart.js');
+
+app.controller("MyVisitors.DashController", function ($scope, $http, $window, $element) {
 
 
     // Set width of graf when we resize
@@ -29,13 +33,26 @@ angular.module("umbraco").controller("MyVisitors.DashController", function ($sco
     $scope.add = function () {
         $scope.sites.push({});
     };
+    $scope.someData = {
+        labels: ['Apr', 'May', 'Jun'],
+        datasets: [
+          {
+              data: [1, 7, 15, 19, 31, 40]
+          }
+        ]
+    };
+
+    $scope.someOptions = {
+        segementStrokeWidth: 20,
+        segmentStrokeColor: '#000'
+    };
 
     $scope.save = function (site) {
 
         if (!site.Id)
             site.Id = "";
 
-        $http.post("backoffice/MyVisitors/Sites/SaveSite?Id="+ site.Id +"&name="+ site.Name +"&siteid="+ site.SiteId +"&sitekey="+ site.SiteKey).success(function (message) {
+        $http.post("backoffice/MyVisitors/Sites/SaveSite?Id=" + site.Id + "&name=" + site.Name + "&siteid=" + site.SiteId + "&sitekey=" + site.SiteKey).success(function (message) {
             GetSites();
         });
 
@@ -49,7 +66,6 @@ angular.module("umbraco").controller("MyVisitors.DashController", function ($sco
 
     };
 
-
     function GetSites() {
 
         $http.get("backoffice/MyVisitors/Sites/GetSites").success(function (data) {
@@ -60,6 +76,20 @@ angular.module("umbraco").controller("MyVisitors.DashController", function ($sco
             $scope.message = null;
 
             angular.forEach(data.sites, function (site) {
+                console.log(site)
+                site.someData = {
+                    labels: ['Apr', 'May', 'Jun'],
+                    datasets: [
+                      {
+                          data: [1, 7, 15, 19, 31, 40]
+                      }
+                    ]
+                };
+
+                site.someOptions = {
+                    segementStrokeWidth: 20,
+                    segmentStrokeColor: '#000'
+                };
 
                 $http.get("backoffice/MyVisitors/Visitors/GetVisitors", { params: { siteid: site.SiteId, sitekey: site.SiteKey } }).success(function (visitors) {
 
@@ -76,4 +106,52 @@ angular.module("umbraco").controller("MyVisitors.DashController", function ($sco
         });
     }
 
+
+    //$scope.load = function() {
+    //  
+    //}
+
 });
+
+
+
+
+
+
+//angular.module("umbraco").directive('myvisitors', function () {
+//    return {
+//        restrict: 'E',
+//        template: '<canvas></canvas><div class="jschart-legend" ng-if="legend" ng-bind-html="legend | sanitize"></div>',
+//        controller: ['$scope', '$element', '$http', '$rootScope', function ($scope, $element, $http, $rootScope) {
+//            var canvas = $element.find('canvas').get(0);
+//            console.log($element)
+//            console.log(canvas)
+//            var ctx = canvas.getContext("2d");
+
+//            var options = {
+//                scaleShowHorizontalLines: false,
+//                scaleShowVerticalLines: false,
+//                legendTemplate: "<div class=\"jschart-legend-line\"><% for (var i=0; i<datasets.length; i++){%><div><div class=\"jschart-legend-border\"><div class=\"jschart-legend-fill\" style=\"border:5px solid <%=datasets[i].strokeColor%>;overflow:hidden\"></div></div><%if(datasets[i].label){%><%=datasets[i].label%><%}%></div><%}%></div>"
+//            };
+
+//            var data = {
+//                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+//                datasets: []
+//            };
+
+//            data.datasets.push({
+//                label: "besökare",
+//                fillColor: "rgba(151,187,205,0.2)",
+//                pointStrokeColor: "#fff",
+//                pointHighlightFill: "#fff",
+//                //pointHighlightStroke: dataset.strokeColor,
+//                data: [12, 19, 3, 5, 2, 3]
+//            });
+//            // Activate chart
+//            var myNewChart = new Chart(ctx).Line(data, options);
+
+//            // Fill legend-div on scope
+//            $scope.legend = myNewChart.generateLegend();
+//        }]
+//    }
+//});
--- Boilerplate/scripts/nav.js
+++ Boilerplate/scripts/nav.js
@@ -1,10 +1,8 @@
 ﻿Camelonta.Nav = (function () {
-    var menuOpen = false,
-        menuOpenClass = 'mobile-menu-open',
+    var menuOpenClass = 'mobile-menu-open',
         expandedClass = 'nav--is-expanded',
         loadingClass = 'nav--is-loading';
 
-    // Get children from the surfacecontroller
     function openSubmenu(e) {
         e.preventDefault();
         var navSubExpander = $(e.target),
@@ -12,19 +10,35 @@
             id = li.data('id'),
             hasSubmenu = li.find('ul').length > 0;
 
+        if (hasSubmenu) {
+            var ul = li.find('ul').first(),
+    isOpen = li.hasClass(expandedClass),
+    actionClass = isOpen ? 'nav--is-minimizing' : 'nav--is-maximizing';
 
-        if (!hasSubmenu) {
+            // ActionClass is used to add the "active" style before the slide-animation is complete.
+            li.addClass(actionClass);
+
+            ul.slideToggle(140, function () {
+                // Remove display after the menu has slided
+                ul.css('display', '');
+                li.toggleClass(expandedClass);
+                li.removeClass(actionClass);
+            });
+          
+        } else {
+            // If the clicked expander has submenus but they do not exist in DOM - fetch them via AJAX
             li.addClass(loadingClass);
 
             var parentUl = navSubExpander.closest('ul'),
-                level = parentUl.data('level'),
-                type = parentUl.data('type');
+                level = parentUl.data('level'), 
+                type = parentUl.data('type'); // Type = main or side (nav)
 
+            // Get children from the surfacecontroller
             $.post('/umbraco/surface/navigationsurface/getsubmenus', { id: id, currentNode: Camelonta.Helper.GetCurrentNodeId(), level: level, type: type }, function (data) {
                 var ul = $(data);
                 li.append(ul);
 
-                ul.slideDown(200, function () {
+                ul.slideDown(140, function () {
                     // Remove display:block after the menu has slided
                     ul.css('display', '');
                 });
@@ -32,29 +46,12 @@
                 li.removeClass(loadingClass);
                 li.addClass(expandedClass);
             });
-        } else {
-            var ul = li.find('ul').first(),
-                isOpen = li.hasClass(expandedClass),
-                actionClass = isOpen ? 'minimizing' : 'maximizing';
-
-            // ActionClass is used to add the "active" style before the slide-animation is complete.
-            li.addClass(actionClass);
-
-            ul.slideToggle(200, function () {
-                // Remove display after the menu has slided
-                ul.css('display', '');
-                li.toggleClass(expandedClass);
-                li.removeClass(actionClass);
-            });
         }
     }
 
 
     // Button to open mobile menu
     $(".mobile-menu-toggle").click(function () {
-
-        menuOpen = !menuOpen; // Change the sate of menuOpen
-
         $('body').toggleClass(menuOpenClass);
     });
 
