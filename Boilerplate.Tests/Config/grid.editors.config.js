d0e204e0fdad01e15cd63e4ddd670d7479f29818
jesse <jesse@d252d4b6-3862-8246-84e1-cf8c128d3774>
2016-09-06 12:12:27 +0000

    boilerplate tests: - updated to 7.5.3

--- Boilerplate.Tests/Config/grid.editors.config.js
+++ Boilerplate.Tests/Config/grid.editors.config.js
@@ -1,12 +1,12 @@
 ﻿[
 	{
-	    "name": "Text",
+	    "name": "Rich text editor",
 	    "alias": "rte",
 	    "view": "rte",
 	    "icon": "icon-article"
 	},
 	{
-	    "name": "Bild",
+	    "name": "Image",
 	    "alias": "media",
 	    "view": "media",
 	    "icon": "icon-picture"
@@ -17,13 +17,30 @@
 	    "view": "macro",
 	    "icon": "icon-settings-alt"
 	},
-   {
-       "name": "Youtube",
-       "alias": "Youtubevideo",
-       "view": "macro",
-       "icon": "icon-play",
-       "config": {
-           "macroAlias": "Youtubevideo"
-       }
-   }
+	{
+	    "name": "Embed",
+	    "alias": "embed",
+	    "view": "embed",
+	    "icon": "icon-movie-alt"
+	},
+    {
+        "name": "Headline",
+        "alias": "headline",
+        "view": "textstring",
+        "icon": "icon-coin",
+        "config": {
+            "style": "font-size: 36px; line-height: 45px; font-weight: bold",
+            "markup": "<h1>#value#</h1>"
+        }
+    },
+	{
+	    "name": "Quote",
+	    "alias": "quote",
+	    "view": "textstring",
+	    "icon": "icon-quote",
+	    "config": {
+	        "style": "border-left: 3px solid #ccc; padding: 10px; color: #ccc; font-family: serif; font-variant: italic; font-size: 18px",
+	        "markup": "<blockquote>#value#</blockquote>"
+	    }
+	}
 ]
