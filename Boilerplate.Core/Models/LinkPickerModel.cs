1f5d2fe7e920e7cdcde07eee62bc22810095a95b
karbel <karbel@d252d4b6-3862-8246-84e1-cf8c128d3774>
2017-04-05 14:04:49 +0000

    Boilerplate: Made a linkpicker.

---
+++ Boilerplate.Core/Models/LinkPickerModel.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boilerplate.Core.Models
+{
+    public class LinkPickerModel
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("url")]
+        public string Url { get; set; }
+        [JsonProperty("target")]
+        public string Target { get; set; }
+    }
+}
