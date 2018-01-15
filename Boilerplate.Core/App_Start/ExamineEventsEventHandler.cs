d02bdcfd625d17227fb5f7a0d31fe016664b0abe
MrSimon <simon.elmqvist@gmail.com>
2018-01-15 14:07:52 +0100

    Umbraco 7.7.8

--- Boilerplate.Core/App_Start/ExamineEventsEventHandler.cs
+++ Boilerplate.Core/App_Start/ExamineEventsEventHandler.cs
@@ -7,14 +7,7 @@ namespace Boilerplate.Core
 {
     public class ExamineEventsEventHandler : IApplicationEventHandler
     {
-        public void OnApplicationInitialized(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
-        {
-            var provider = ExamineManager.Instance.IndexProviderCollection["ExternalIndexer"];
-            if (provider != null)
-            {
-                provider.NodeIndexing += NodeIndexing;
-            }
-        }
+        public void OnApplicationInitialized(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext) { }
 
         private void NodeIndexing(object sender, IndexingNodeEventArgs e)
         {
@@ -22,14 +15,20 @@ namespace Boilerplate.Core
             e.Cancel = DisallowSearchEngineIndexing(e.Fields);
         }
 
-
         private static bool DisallowSearchEngineIndexing(IDictionary<string, string> fields)
         {
             var field = fields.SingleOrDefault(f => f.Key == "robotsIndex");
             return field.Equals(default(KeyValuePair<string, string>)) || field.Value == "1"; // 1 = true, 0 = false
         }
 
-        public void OnApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext) { }
+        public void OnApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            var provider = ExamineManager.Instance.IndexProviderCollection["ExternalIndexer"];
+            if (provider != null)
+            {
+                provider.NodeIndexing += NodeIndexing;
+            }
+        }
 
         public void OnApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext) { }
     }
